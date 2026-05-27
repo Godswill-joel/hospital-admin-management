8d6bcb35558fff465de3faa07181c43b9b9decf8
Godswill-joel <ge42217@gmail.com>
2026-05-27 01:17:50 +0100

    add server.js and updated package.json to use nodemon for development

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//Middleware 
+app.use(cors());
+
+
+//DB
+
+
+
+
+//routes
+app.get("/", (req, res) => {
+    res.send("API working properly");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
